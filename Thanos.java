6cc3545c130435c5debd4bf0cf14b05736c68c37
elisechldn <elise.chaldjian@gmail.com>
2024-07-23 23:03:41 +0200

    First attempt working with Collections : List

--- Thanos.java
+++ Thanos.java
@@ -5,6 +5,7 @@ public class Thanos {
     public static void main(String[] args) {
 
         // TODO 1 : Create an empty heroes list
+        ArrayList<Hero> heroes = new ArrayList<>();
 
         // TODO 2 : Add those heroes to the list
         // name: Black Widow, age: 34
@@ -15,13 +16,33 @@ public class Thanos {
         // name: Thor, age: 1500
         // name: Hulk, age: 49
         // name: Doctor Strange, age: 42
+        heroes.add(new Hero("Black Widow", 34));
+        heroes.add(new Hero("Captain America", 100));
+        heroes.add(new Hero("Vision", 3));
+        heroes.add(new Hero("Iron Man", 48));
+        heroes.add(new Hero("Scarlet Witch", 29));
+        heroes.add(new Hero("Thor", 1500));
+        heroes.add(new Hero ("Hulk", 49));
+        heroes.add(new Hero("Doctor Strange", 42));
 
         // TODO 3 : It's Thor birthday, now he's 1501
-
+        
+        for (int i = 0; i < heroes.size(); i++) {
+            Hero thor = heroes.get(i);
+            if (thor.getName().equals("Thor")) {
+                thor.setAge(thor.getAge() + 1);
+            }
+        }
         // TODO 4 : Shuffle the heroes list
-
+        Collections.shuffle(heroes);
+        
         // TODO 5 : Keep only the half of the list
+        int midList = heroes.size() / 2;
+        List<Hero> remainingHeroes = heroes.subList(0, midList);
 
         // TODO 6 : Loop throught the list and display the name of the remaining heroes
+        for (Hero hero : remainingHeroes) {
+            System.out.println(hero.getName());
+        }
     }
 }
